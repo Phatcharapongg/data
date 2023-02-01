ece3dc30cc9a5e61e77cc3d561b501ed8dbea428
Phatcharapongg <momark2601@gmail.com>
2023-02-01 16:09:57 +0700

    asdad

--- config/config.php
+++ config/config.php
@@ -4,8 +4,8 @@ date_default_timezone_set("Asia/Bangkok");
 
 $dataNow = date("Y-m-d H:i:s");
 
-$_SESSION['uri'] = 'http://localhost';
-$path = 'deposit';
+$_SESSION['uri'] = 'http://swdata.srisangworn.go.th';
+$path = 'Deposit';
 
 $servername = "192.168.0.32";
 $username = "ict009";
