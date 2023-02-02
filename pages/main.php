4abf76b30bcae8d830b3f671ab96c6c8ede50679
Phatcharapongg <momark2601@gmail.com>
2023-02-02 16:36:01 +0700

    020266

--- pages/main.php
+++ pages/main.php
@@ -119,7 +119,7 @@ if ($getUserNUM == 1) {
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="<?= $_SESSION['uri']; ?>/<?= $path; ?>/pages/main.php?path=dashboard" class="brand-link">
-                <img src="../dist/img/2.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <img src="../dist/img/8.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">Student Deposit</span>
             </a>
             <!-- Student Deposit -->
